f2b67c27462d4564d8cc59ede603cc10f97d6506
name-88 <sasha33433@gmail.com>
2024-10-01 20:17:22 +0300

    изменен формат вывода курса, добавлена функция window.mainloop

--- exchange.py
+++ exchange.py
@@ -1,5 +1,5 @@
 from http.client import responses
-import request
+import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
@@ -12,9 +12,10 @@ def exchange():
         try:
             response = requests.get('http://open.er-api.com/v6/latest/USD')
             response.raise_for_status()
+            data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate}{code} за 1 доллар")
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f}{code} за 1 доллар")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена!")
         except Exception as e:
@@ -25,7 +26,7 @@ def exchange():
     # Рисуем главное окно
 window = Tk()
 window.title("Курсы валют")
-window.geometry("360x180)
+window.geometry("360x180")
 
 Label(text="Введите код валюты").pack(padx=10, pady=10)
 
@@ -33,3 +34,5 @@ entry = Entry()
 entry.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
+
+window.mainloop()

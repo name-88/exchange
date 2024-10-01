1c09de4fbf501bc6193b11d60b615565bc7f544b
name-88 <sasha33433@gmail.com>
2024-10-01 19:54:52 +0300

    добавлена функция exchange

--- exchange.py
+++ exchange.py
@@ -1,9 +1,28 @@
+from http.client import responses
 import request
 import json
 from tkinter import *
 from tkinter import messagebox as mb
 
+    # Функция exchange
+def exchange():
+    code = entry.get()
 
+    if code:
+        try:
+            response = requests.get('http://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate}{code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты!")
+
+    # Рисуем главное окно
 window = Tk()
 window.title("Курсы валют")
 window.geometry("360x180)

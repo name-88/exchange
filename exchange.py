75b78deab3550c18d70a097c78e0b4a86538685f
name-88 <sasha33433@gmail.com>
2024-10-02 11:54:51 +0300

    вторая базовая валюта

--- exchange.py
+++ exchange.py
@@ -10,32 +10,52 @@ def update_b_label(event):
     name = cur.get(code)
     b_label.config(text=name)
 
+# Функция для метки второй базовой валюты
+def update_bb_label(event):
+    code = bb_combobox.get()
+    name = cur.get(code)
+    bb_label.config(text=name)
+
 # Функция для обновления метки с названием валюты
 def update_t_label(event):
     code = t_combobox.get()
     name = cur.get(code)
     t_label.config(text=name)
 
+
 # Функция exchange
 def exchange():
     t_code = t_combobox.get()
     b_code = b_combobox.get()
-    if t_code and b_code:
+    bb_code = bb_combobox.get()
+
+    if t_code and b_code and bb_code:
         try:
             response = requests.get(f'http://open.er-api.com/v6/latest/{b_code}')
             response.raise_for_status()
-            data = response.json()
-            if t_code in data['rates']:
-                exchange_rate = data['rates'][t_code]
+            data_b = response.json()
+
+            response = requests.get(f'http://open.er-api.com/v6/latest/{bb_code}')
+            response.raise_for_status()
+            data_bb = response.json()
+
+            if t_code in data_b['rates'] and t_code in data_bb['rates']:
+                exchange_rate_b = data_b['rates'][t_code]
+                exchange_rate_bb = data_bb['rates'][t_code]
+
                 t_name = cur[t_code]
                 b_name = cur[b_code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {t_name} за 1 {b_name}")
+                bb_name = cur[bb_code]
+
+                mb.showinfo("Курсы обмена",
+                            f"Курс: {exchange_rate_b:.2f} {t_name} за 1 {b_name}\n"
+                            f"Курс: {exchange_rate_bb:.2f} {t_name} за 1 {bb_name}")
             else:
-                mb.showerror("Ошибка", f"Валюта {t_code} не найдена!")
+                mb.showerror("Ошибка", f"Валюта {t_code} не найдена в одной из базовых валют!")
         except Exception as e:
-                mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
     else:
-            mb.showwarning("Внимание!", "Введите код валюты!")
+        mb.showwarning("Внимание!", "Введите код валюты!")
 
 # Словарь с кодами валют
 cur = {
@@ -54,17 +74,27 @@ cur = {
 # Рисуем главное окно
 window = Tk()
 window.title("Курсы валют")
-window.geometry("360x300")
+window.geometry("360x500")
 
 # Метка базовой валюты
 b_label = ttk.Label()
 b_label.pack(padx=10, pady=10)
 
-# список базовой валюты
+# Метка второй базовой валюты
+bb_label = ttk.Label()
+bb_label.pack(padx=10, pady=10)
+
+# Список базовой валюты
 Label(text="Базовая валюта").pack(padx=10, pady=10)
 b_combobox = ttk.Combobox(value=list(cur.keys()))
 b_combobox.pack(padx=10, pady=10)
 b_combobox.bind("<<ComboboxSelected>>", update_b_label)
+
+# Список второй базовой валюты
+Label(text="Вторая базовая валюта").pack(padx=10, pady=10)
+bb_combobox = ttk.Combobox(value=list(cur.keys()))
+bb_combobox.pack(padx=10, pady=10)
+bb_combobox.bind("<<ComboboxSelected>>", update_bb_label)
 
 
 # Список целевой валюты
